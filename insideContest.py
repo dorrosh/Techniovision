2700da4c3799e8773131f4b7055437945f60ddb2
ROSH <dor.rosh@campus.technion.ac.il>
2019-05-07 12:48:14 +0300

    version 7-5-19.

--- insideContest.py
+++ insideContest.py
@@ -1,45 +1,44 @@
-import os
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
 STUDENT_ID = 2
 FACULTY_NAME = -1
 STUDENT_VOTE = 3
-STAFF_VOTE = 20
-FIRST_STAFF_VOTE = 2
+STAFF_SCORE = 20
+STAFF_VOTE = 2
 OTHER_FACULTY_PROGRAMS = 3
 
 
 def inside_contest(faculty, file_name):
-    faculty_programs = creatFacultyProgramsList(faculty, file_name)
+    facultyPrograms = creatFacultyProgramsList(faculty, file_name)
     file_name = open(file_name, 'r')
     votersIds = []
     for line in file_name:
-        programs_name_vote = line.split()
-        if programs_name_vote[0] == "inside" and programs_name_vote[1] == "contest":
-            if programs_name_vote[STUDENT_ID] in votersIds or programs_name_vote[FACULTY_NAME] != faculty:
+        programNameVote = line.split()
+        if programNameVote[0] == "inside" and programNameVote[1] == "contest":
+            if programNameVote[STUDENT_ID] in votersIds or programNameVote[FACULTY_NAME] != faculty:
                 continue
-            votersIds.append(programs_name_vote[STUDENT_ID])
-            for iterator in faculty_programs:
-                if programs_name_vote[STUDENT_VOTE] == iterator[0]:
+            votersIds.append(programNameVote[STUDENT_ID])
+            for iterator in facultyPrograms:
+                if programNameVote[STUDENT_VOTE] == iterator[0]:
                     iterator[1] += 1
-    print(faculty_programs)
-    name = findWinnerProgram(faculty_programs)
+    print(facultyPrograms)
+    name = findWinnerProgram(facultyPrograms)
     return name
 
 
 def creatFacultyProgramsList(faculty, file_name):
     file_name = open(file_name, 'r')
-    faculty_programs = []
+    facultyPrograms = []
     for line in file_name:
-        programs_name = line.split()
-        if programs_name[0] == "staff" and programs_name[1] == "choice":
-            if programs_name[FACULTY_NAME] == faculty:
-                len1 = len(programs_name)
-                faculty_programs.append([programs_name[FIRST_STAFF_VOTE], STAFF_VOTE])
-                for i in range(OTHER_FACULTY_PROGRAMS, len1 - 1):
-                    faculty_programs.append([programs_name[i], 0])
+        programName = line.split()
+        if programName[0] == "staff" and programName[1] == "choice":
+            if programName[FACULTY_NAME] == faculty:
+                lineLength = len(programName)
+                facultyPrograms.append([programName[STAFF_VOTE], STAFF_SCORE])
+                for i in range(OTHER_FACULTY_PROGRAMS, lineLength - 1):
+                    facultyPrograms.append([programName[i], 0])
                 break
     file_name.close()
-    return faculty_programs
+    return facultyPrograms
 
 
 def findWinnerProgram(faculty_programs):
@@ -54,8 +53,9 @@ def findWinnerProgram(faculty_programs):
 
 
 def main():
-    my_file = os.path.join(THIS_FOLDER, 'test.txt')
-    print(str(inside_contest("cs", my_file)))
+
+    print(str(inside_contest("cs", "test.txt")))
+
 
 
 if __name__ == '__main__':
